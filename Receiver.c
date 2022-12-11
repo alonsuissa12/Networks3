51024512189b7646578d24afe5980ec1fe5473e2
alon suissa <alonsuissa12@gmail.com>
2022-12-11 23:48:04 +0200

    readed file 11/12/22

--- Receiver.c
+++ Receiver.c
@@ -4,24 +4,37 @@
 #define port 8888
 #define IP "127.0.0.1"
 #include "Receiver.h"
-void main(){
+int main() {
     int sock = socket(AF_INET, SOCK_STREAM, 0);
-    struct sockaddr_in receiver_adderess;
-    memset(&receiver_adderess,0,sizeof(receiver_adderess));
-    receiver_adderess.sin_family = AF_INET;
-    receiver_adderess.sin_port = htons(port);
-    int checkP = inet_pton(AF_INET,(const char*)IP,&receiver_adderess.sin_addr);
-    if(checkP <= 0){
+    struct sockaddr_in senderAddress;
+    memset(&senderAddress, 0, sizeof(senderAddress)); //not necessary to insert si_zero because everything is 0 now.
+    senderAddress.sin_family = AF_INET;
+    senderAddress.sin_port = htons(port);
+    int checkP = inet_pton(AF_INET, (const char *) IP, &senderAddress.sin_addr);
+    if (checkP <= 0) {
         printf("inet_pton() failed");
     }
-    int Bcheck = bind(sock,(struct sockaddr *)&receiver_adderess ,sizeof(receiver_adderess));
-    if(Bcheck == -1){
+    int Bcheck = bind(sock, (struct sockaddr *) &senderAddress, sizeof(senderAddress));
+    if (Bcheck == -1) {
         printf("error while binding");
     }
-    int Lcheck = listen(sock,1);
-    if(Lcheck == -1){
+    int Lcheck = listen(sock, 1);
+    if (Lcheck == -1) {
         printf("error in listen()");
     }
+    while(1) {
+        memset(&senderAddress, 0, sizeof(senderAddress));
+        int senderAddressLen = sizeof(senderAddress);
+        int senderSocket = accept(sock, (struct sockaddr *) &senderAddress, &senderAddressLen);
+        if(senderSocket == -1){
+            printf("accept() failed");
+            // need to close the socket
+            return -1;
+        }
 
+
+
+
+    }
 
 }
--- Sender.c
+++ Sender.c
@@ -1,31 +1,58 @@
 //
 // Created by alon on 12/7/22.
 //
+#include <sys/stat.h>
 #include "Sender.h"
 #define port 8888
 #define IP "127.0.0.1"
 
 int main(){
-    //
-    //need to read the file!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    //
+    FILE * fPointer;
+    fPointer = fopen( "2mb_file.txt","r");
+    struct stat st;
+    stat("2mb_file.txt", &st);
+    int size = st.st_size;
+    //assuming that the size is even (NEED TO CHECK FOR ODD)
+    int halfSize = size/2;
+    char firstHalf[halfSize];
+    fgets(firstHalf, halfSize ,fPointer);
+    if(size % 2 == 1){
+        char secondHalf[halfSize +1];
+        fgets(secondHalf, halfSize + 1, fPointer + halfSize);
+    }
+    else {
+        char secondHalf[halfSize];
+        fgets(secondHalf, halfSize, fPointer + halfSize);
+    }
+
     int sock = socket(AF_INET, SOCK_STREAM, 0);
     if(sock == -1) {
         printf("Could not create socket");
     }
-    //sockaddr - stract of adress and port
-    //the size of ^
-    struct sockaddr_in senderAddress;
-    memset(&senderAddress,0,sizeof(senderAddress)); //not necessary to insert si_zero because everything is 0 now.
-    senderAddress.sin_family = AF_INET;
-    senderAddress.sin_port = htons(port);
-    int checkP = inet_pton(AF_INET,(const char*)IP,&senderAddress.sin_addr);
+    struct sockaddr_in receiver_adderess;
+    memset(&receiver_adderess,0,sizeof(receiver_adderess));
+    receiver_adderess.sin_family = AF_INET;
+    receiver_adderess.sin_port = htons(port);
+    int checkP = inet_pton(AF_INET,(const char*)IP,&receiver_adderess.sin_addr);
     if(checkP <= 0){
         printf("inet_pton() failed");
     }
-    int connectCheck = connect(sock,(struct sockaddr*) &senderAddress,sizeof(senderAddress));
+
+    int connectCheck = connect(sock,(struct sockaddr*) &receiver_adderess,sizeof(receiver_adderess));
     if(connectCheck == -1){
         printf("connect() failed");
     }
+    //setsockopt(sock  );
+//send the first part
+//
+//change CC algorithm.
+//
+//send the second part
+//
+// do u want to send again?
+//
+//
 
+
+//close the file
 }
